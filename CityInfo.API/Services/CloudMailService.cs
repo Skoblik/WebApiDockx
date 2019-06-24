d60cb8f3b332cbb2132fae9eb9921f59249fc87a
Yurii Andrushchenko <yandru@softserveinc.com>
2019-06-24 18:45:10 +0300

    Starting part seven

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -8,10 +8,10 @@ namespace CityInfo.API.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
+        private string _mailTo = Startup.Configuration["mailSettings:mailToAddress"];
+		private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
 
-        public void Send(string subject, string message)
+		public void Send(string subject, string message)
         {
             Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with CloudMailService.");
             Debug.WriteLine($"Subject: {subject}");
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Entities;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
@@ -16,6 +19,19 @@ namespace CityInfo.API
 {
 	public class Startup
 	{
+		public static IConfigurationRoot Configuration;
+
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+				.AddEnvironmentVariables();
+
+			Configuration = builder.Build();
+		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
@@ -23,24 +39,27 @@ namespace CityInfo.API
 			services.AddMvc()
 				.AddMvcOptions(o => o.OutputFormatters.Add(
 					new XmlDataContractSerializerOutputFormatter()));
-            //.AddJsonOptions(o => {
-            //	if (o.SerializerSettings.ContractResolver != null)
-            //	{
-            //		var castedResolver = o.SerializerSettings.ContractResolver
-            //			as DefaultContractResolver;
-            //		castedResolver.NamingStrategy = null;
-            //	}
-            //});
+			//.AddJsonOptions(o => {
+			//	if (o.SerializerSettings.ContractResolver != null)
+			//	{
+			//		var castedResolver = o.SerializerSettings.ContractResolver
+			//			as DefaultContractResolver;
+			//		castedResolver.NamingStrategy = null;
+			//	}
+			//});
 #if DEBUG
-            services.AddTransient<IMailService, LocalMailService>();
+			services.AddTransient<IMailService, LocalMailService>();
 #else
             services.AddTransient<IMailService, CloudMailService>();
 #endif
+			var connectionString = Startup.Configuration["connectionStrings:cityInfoDBConnectionString"];
+			services.AddDbContext<CityInfoContext>(o => o.UseSqlServer(connectionString));
 
+			services.AddScoped<ICityInfoRepository, CityInfoRepository>();
         }
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, CityInfoContext cityInfoContext)
 		{
 			loggerFactory.AddConsole();
 
@@ -49,7 +68,7 @@ namespace CityInfo.API
 			//loggerFactory.AddProvider(new NLog.Extensions.Logging.NLogLoggerProvider());
 
 			loggerFactory.AddNLog();
-
+			
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
@@ -58,6 +77,8 @@ namespace CityInfo.API
 			{
 				app.UseExceptionHandler();
 			}
+
+			cityInfoContext.EnsureSeeDataForContext();
 
 			app.UseStatusCodePages();
 
